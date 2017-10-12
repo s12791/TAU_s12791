b16f7d7febdd50a271c91de7bd18baff4e450c1e
s12791 <kamilkoczkowski@wp.pl>
2017-10-12 21:42:50 +0200

    Added minor change of test implementation

--- SpaceMarineApp/src/test/java/s12791/service/SpaceMarineManagerTest.java
+++ SpaceMarineApp/src/test/java/s12791/service/SpaceMarineManagerTest.java
@@ -9,50 +9,49 @@ import s12791.domain.SpaceMarine;
 
 @RunWith(JUnit4.class)
 public class SpaceMarineManagerTest extends TestCase {
-SpaceMarineManager spaceMarineManager;
 
-SpaceMarineManagerImpl impl = new SpaceMarineManagerImpl();
+SpaceMarineManager spaceMarineManager = new SpaceMarineManagerImpl();
 
 	@Test
 	public void testCreateSpaceMarine() throws Exception{
 		SpaceMarine sm1= new SpaceMarine("Gabriel", "Thunder Hammer", 5, 5, "Blood Ravens");
-		assertEquals(1,impl.createSpaceMarine(sm1));
+		assertEquals(1,spaceMarineManager.createSpaceMarine(sm1));
 	}
 	@Test
 	public void testGetSpaceMarine() throws Exception{
 		SpaceMarine sm2= new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
-		impl.createSpaceMarine(sm2);
-		assertEquals("Tarkus",impl.getSpaceMarine(0).getName());
-		assertEquals("Bolter",impl.getSpaceMarine(0).getWeapon());
-		assertEquals(4,impl.getSpaceMarine(0).getBallisticSkill());
-		assertEquals(4,impl.getSpaceMarine(0).getWeaponSkill());
-		assertEquals("Blood Ravens",impl.getSpaceMarine(0).getChapter());
+		spaceMarineManager.createSpaceMarine(sm2);
+		assertEquals("Tarkus",spaceMarineManager.getSpaceMarine(0).getName());
+		assertEquals("Bolter",spaceMarineManager.getSpaceMarine(0).getWeapon());
+		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getBallisticSkill());
+		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getWeaponSkill());
+		assertEquals("Blood Ravens",spaceMarineManager.getSpaceMarine(0).getChapter());
 	}
 	@Test
 	public void testUpdateSpaceMarine() throws Exception{
 		SpaceMarine sm3= new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
 		SpaceMarine sm4= new SpaceMarine("Thaddeus", "Chainsword", 4, 4, "Blood Ravens");
 
-		impl.createSpaceMarine(sm3);
+		spaceMarineManager.createSpaceMarine(sm3);
 
-		assertEquals(1,impl.updateSpaceMarine(sm4,0));
+		assertEquals(1,spaceMarineManager.updateSpaceMarine(sm4,0));
 		
-		assertEquals("Thaddeus",impl.getSpaceMarine(0).getName());
-		assertEquals("Chainsword",impl.getSpaceMarine(0).getWeapon());
-		assertEquals(4,impl.getSpaceMarine(0).getBallisticSkill());
-		assertEquals(4,impl.getSpaceMarine(0).getWeaponSkill());
-		assertEquals("Blood Ravens",impl.getSpaceMarine(0).getChapter());
+		assertEquals("Thaddeus",spaceMarineManager.getSpaceMarine(0).getName());
+		assertEquals("Chainsword",spaceMarineManager.getSpaceMarine(0).getWeapon());
+		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getBallisticSkill());
+		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getWeaponSkill());
+		assertEquals("Blood Ravens",spaceMarineManager.getSpaceMarine(0).getChapter());
 	}
 	@Test
 	public void testDeleteSpaceMarine() throws Exception{
 		SpaceMarine sm1= new SpaceMarine("Gabriel", "Thunder Hammer", 5, 5, "Blood Ravens");
 		SpaceMarine sm2= new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
 		SpaceMarine sm3= new SpaceMarine("Thaddeus", "Chainsword", 4, 4, "Blood Ravens");
-		impl.createSpaceMarine(sm1);
-		impl.createSpaceMarine(sm2);
-		impl.createSpaceMarine(sm3);
+		spaceMarineManager.createSpaceMarine(sm1);
+		spaceMarineManager.createSpaceMarine(sm2);
+		spaceMarineManager.createSpaceMarine(sm3);
 		
-		assertEquals(2,impl.deleteSpaceMarine(1));
+		assertEquals(2,spaceMarineManager.deleteSpaceMarine(1));
 	}
 	
 	
