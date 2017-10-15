0c8b3b743c184d3d4cb1ddb9d2eb29edd25bdd66
s12791 <kamilkoczkowski@wp.pl>
2017-10-15 10:27:01 +0200

    Corrected issues, added assertion to testDeleteSpaceMarine()

--- SpaceMarineApp/src/main/java/s12791/domain/SpaceMarine.java
+++ SpaceMarineApp/src/main/java/s12791/domain/SpaceMarine.java
@@ -1,70 +1,77 @@
 package s12791.domain;
 
 public class SpaceMarine {
-	
-private long id;
-private String name;
-private String weapon;
-private int weaponSkill;
-private int ballisticSkill;
-private String chapter;
 
+	private long id;
+	private String name;
+	private String weapon;
+	private int weaponSkill;
+	private int ballisticSkill;
+	private String chapter;
 
+	@Override
+	public String toString() {
+		return "SpaceMarine [id=" + id + ", name=" + name + ", weapon=" + weapon + ", weaponSkill=" + weaponSkill
+				+ ", ballisticSkill=" + ballisticSkill + ", chapter=" + chapter + "]";
+	}
 
-@Override
-public String toString() {
-	return "SpaceMarine [id=" + id + ", name=" + name + ", weapon=" + weapon + ", weaponSkill=" + weaponSkill
-			+ ", ballisticSkill=" + ballisticSkill + ", chapter=" + chapter + "]";
-}
-public SpaceMarine(String name, String weapon, int weaponSkill, int ballisticSkill, String chapter) {
-	super();
-	this.name = name;
-	this.weapon = weapon;
-	this.weaponSkill = weaponSkill;
-	this.ballisticSkill = ballisticSkill;
-	this.chapter = chapter;
-}
-public SpaceMarine() {
-	
-}
+	public SpaceMarine(String name, String weapon, int weaponSkill, int ballisticSkill, String chapter) {
+		this.name = name;
+		this.weapon = weapon;
+		this.weaponSkill = weaponSkill;
+		this.ballisticSkill = ballisticSkill;
+		this.chapter = chapter;
+	}
 
-public long getId() {
-	return id;
-}
-public void setId(long id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getWeapon() {
-	return weapon;
-}
-public void setWeapon(String weapon) {
-	this.weapon = weapon;
-}
-public int getWeaponSkill() {
-	return weaponSkill;
-}
-public void setWeaponSkill(int weaponSkill) {
-	this.weaponSkill = weaponSkill;
-}
-public int getBallisticSkill() {
-	return ballisticSkill;
-}
-public void setBallisticSkill(int ballisticSkill) {
-	this.ballisticSkill = ballisticSkill;
-}
-public String getChapter() {
-	return chapter;
-}
-public void setChapter(String chapter) {
-	this.chapter = chapter;
-}
+	public SpaceMarine() {
+	}
 
+	public long getId() {
+		return id;
+	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getWeapon() {
+		return weapon;
+	}
+
+	public void setWeapon(String weapon) {
+		this.weapon = weapon;
+	}
+
+	public int getWeaponSkill() {
+		return weaponSkill;
+	}
+
+	public void setWeaponSkill(int weaponSkill) {
+		this.weaponSkill = weaponSkill;
+	}
+
+	public int getBallisticSkill() {
+		return ballisticSkill;
+	}
+
+	public void setBallisticSkill(int ballisticSkill) {
+		this.ballisticSkill = ballisticSkill;
+	}
+
+	public String getChapter() {
+		return chapter;
+	}
+
+	public void setChapter(String chapter) {
+		this.chapter = chapter;
+	}
 
 }
--- SpaceMarineApp/src/test/java/s12791/service/SpaceMarineManagerTest.java
+++ SpaceMarineApp/src/test/java/s12791/service/SpaceMarineManagerTest.java
@@ -10,48 +10,53 @@ import s12791.domain.SpaceMarine;
 @RunWith(JUnit4.class)
 public class SpaceMarineManagerTest extends TestCase {
 
-SpaceMarineManager spaceMarineManager = new SpaceMarineManagerImpl();
+	SpaceMarineManager spaceMarineManager = new SpaceMarineManagerImpl();
+
 	@Test
-	public void testCreateSpaceMarine() throws Exception{
-		SpaceMarine sm1= new SpaceMarine("Gabriel", "Thunder Hammer", 5, 5, "Blood Ravens");
-		assertEquals(1,spaceMarineManager.createSpaceMarine(sm1));
+	public void testCreateSpaceMarine() throws Exception {
+		SpaceMarine sm1 = new SpaceMarine("Gabriel", "Thunder Hammer", 5, 5, "Blood Ravens");
+		assertEquals(1, spaceMarineManager.createSpaceMarine(sm1));
 	}
+
 	@Test
-	public void testGetSpaceMarine() throws Exception{
-		SpaceMarine sm2= new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
+	public void testGetSpaceMarine() throws Exception {
+		SpaceMarine sm2 = new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
 		spaceMarineManager.createSpaceMarine(sm2);
-		assertEquals("Tarkus",spaceMarineManager.getSpaceMarine(0).getName());
-		assertEquals("Bolter",spaceMarineManager.getSpaceMarine(0).getWeapon());
-		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getBallisticSkill());
-		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getWeaponSkill());
-		assertEquals("Blood Ravens",spaceMarineManager.getSpaceMarine(0).getChapter());
+		assertEquals("Tarkus", spaceMarineManager.getSpaceMarine(0).getName());
+		assertEquals("Bolter", spaceMarineManager.getSpaceMarine(0).getWeapon());
+		assertEquals(4, spaceMarineManager.getSpaceMarine(0).getBallisticSkill());
+		assertEquals(4, spaceMarineManager.getSpaceMarine(0).getWeaponSkill());
+		assertEquals("Blood Ravens", spaceMarineManager.getSpaceMarine(0).getChapter());
 	}
+
 	@Test
-	public void testUpdateSpaceMarine() throws Exception{
-		SpaceMarine sm3= new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
-		SpaceMarine sm4= new SpaceMarine("Thaddeus", "Chainsword", 4, 4, "Blood Ravens");
+	public void testUpdateSpaceMarine() throws Exception {
+		SpaceMarine sm3 = new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
+		SpaceMarine sm4 = new SpaceMarine("Thaddeus", "Chainsword", 4, 4, "Blood Ravens");
 
 		spaceMarineManager.createSpaceMarine(sm3);
 
-		assertEquals(1,spaceMarineManager.updateSpaceMarine(sm4,0));
-		
-		assertEquals("Thaddeus",spaceMarineManager.getSpaceMarine(0).getName());
-		assertEquals("Chainsword",spaceMarineManager.getSpaceMarine(0).getWeapon());
-		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getBallisticSkill());
-		assertEquals(4,spaceMarineManager.getSpaceMarine(0).getWeaponSkill());
-		assertEquals("Blood Ravens",spaceMarineManager.getSpaceMarine(0).getChapter());
+		assertEquals(1, spaceMarineManager.updateSpaceMarine(sm4, 0));
+
+		assertEquals("Thaddeus", spaceMarineManager.getSpaceMarine(0).getName());
+		assertEquals("Chainsword", spaceMarineManager.getSpaceMarine(0).getWeapon());
+		assertEquals(4, spaceMarineManager.getSpaceMarine(0).getBallisticSkill());
+		assertEquals(4, spaceMarineManager.getSpaceMarine(0).getWeaponSkill());
+		assertEquals("Blood Ravens", spaceMarineManager.getSpaceMarine(0).getChapter());
 	}
+
 	@Test
-	public void testDeleteSpaceMarine() throws Exception{
-		SpaceMarine sm1= new SpaceMarine("Gabriel", "Thunder Hammer", 5, 5, "Blood Ravens");
-		SpaceMarine sm2= new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
-		SpaceMarine sm3= new SpaceMarine("Thaddeus", "Chainsword", 4, 4, "Blood Ravens");
+	public void testDeleteSpaceMarine() throws Exception {
+		SpaceMarine sm1 = new SpaceMarine("Gabriel", "Thunder Hammer", 5, 5, "Blood Ravens");
+		SpaceMarine sm2 = new SpaceMarine("Tarkus", "Bolter", 4, 4, "Blood Ravens");
+		SpaceMarine sm3 = new SpaceMarine("Thaddeus", "Chainsword", 4, 4, "Blood Ravens");
 		spaceMarineManager.createSpaceMarine(sm1);
 		spaceMarineManager.createSpaceMarine(sm2);
 		spaceMarineManager.createSpaceMarine(sm3);
-		
-		assertEquals(2,spaceMarineManager.deleteSpaceMarine(1));
+
+		assertEquals(2, spaceMarineManager.deleteSpaceMarine(1));
+		assertNull(spaceMarineManager.getSpaceMarine(1));
+
 	}
-	
-	
+
 }
